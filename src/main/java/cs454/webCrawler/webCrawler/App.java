608790a52a268ad591831ff049299d9d00b4efcd
RishiSKeshav <rishiskeshav91@gmail.com>
2015-02-25 15:56:20 -0800

    test commit

--- src/main/java/cs454/webCrawler/webCrawler/App.java
+++ src/main/java/cs454/webCrawler/webCrawler/App.java
@@ -9,10 +9,6 @@ public class App
     public static void main( String[] args )
     {
     	Crawl.parse("http://www.ven.lewebdev.com/");
-    	 
-    	
-    	
-    	
     	/*if(args[0]!= null)
     	{
     		int depth = Integer.parseInt(args[1]);
